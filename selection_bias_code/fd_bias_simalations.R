774e087048adb971c015b90e9bce6d859c8cf9d6
FabianObster <fabian.obster57@gmail.com>
2024-08-14 15:53:06 +0200

    Finalize first setting of 5 variables. two more are missing

--- selection_bias_code/fd_bias_simalations.R
+++ selection_bias_code/fd_bias_simalations.R
@@ -4,34 +4,8 @@ library(pracma)
 library(gglasso)
 library(mvtnorm)
 library(furrr)
-####### Fix group_by problem.......
 
-#strategy: big sel -> Move towards balance with learning rate.
-## Helper function Tuning mboost
-group_df <- data.frame(var_name = c('V1', 'V2', 'V3', 'V4', 'V5'),
-                       group_name = as.integer(c(1,1,2,2,3)))
-df <- dat
-
-
-fit_mb_lam <- function(iter, df){
-  formula <- as.formula(y ~bols(V1, V2, lambda = 10)+
-                          bols(V3, V4, lambda = 10) +
-                          bols(V5, lambda = 10))
-  sim_df <- df %>% dplyr::mutate(y = rnorm(dim(df)[1]))
-  mb_model <- mboost(formula = formula, data = sim_df, control = boost_control(mstop = 1))
-  mb_model$xselect()
-}
-fit_mb_df <- function(iter, df){
-  formula <- as.formula(y ~bols(V1, V2, df = 0.5)+
-                          bols(V3, V4,df = 0.5) +
-                          bols(V5,df = 0.5))
-  sim_df <- df %>% dplyr::mutate(y = rnorm(dim(df)[1]))
-  mb_model <- mboost(formula = formula, data = sim_df, control = boost_control(mstop = 1))
-  mb_model$xselect()
-}
-
-
-get_parameters <- function(df, group_df, block_fit, block_size = 1000, max_steps = 30, tol = 0.02, rate = 0.1){
+get_parameters <- function(df, group_df, block_fit, block_size = 1000, max_steps = 50, tol = 0.001, rate = 0.1){
   # Initialization
   scaling <- as.data.frame(t(rep(1,length(unique(group_df$var_name)))))
   colnames(scaling) <- group_df$var_name
@@ -86,16 +60,10 @@ get_parameters <- function(df, group_df, block_fit, block_size = 1000, max_steps
      break
    }
   }
-  return(list('results' = big_results_df, 'scaling' = tail(scaling,1)))
+  return(list('results' = big_results_df, 'scaling' = tail(scaling,1), 'best' = best_scale))
 }
-plan(multisession)
-mb_params_lam <- get_parameters(df = df, group_df = group_df, block_size = 5000, block_fit = fit_mb_lam, rate = 0.8)
-plan(sequential)
-plan(multisession)
-mb_params_df <- get_parameters(df = df, group_df = group_df, block_size = 5000, block_fit = fit_mb_df, rate = 0.8)
-plan(sequential)
 ## Helper function Tuning grlasso
-fit_lasso <- function(iter, df){
+fit_lasso_01 <- function(iter, df){
   
   sim_df <- df %>% dplyr::mutate(y = rnorm(dim(df)[1]))
  
@@ -111,16 +79,62 @@ fit_lasso <- function(iter, df){
     filter(lambda == gr_lasso_coef$lambda[1])
   gr_lasso_coef
   gr_lasso_coef <- gr_lasso_coef %>% 
-    mutate(sel = case_when(any(rowname %in% c('V1','V2'))~ 1, 
-                           any(rowname %in% c('V3','V4'))~2, 
-                           any(rowname %in% c('V5'))~ 3))
-  gr_lasso_coef$sel
+    mutate(sel = case_when(rowname %in% c('V1','V2')~ 1, 
+                           rowname %in% c('V3','V4')~2, 
+                           rowname %in% c('V5')~ 3))
+  unique(gr_lasso_coef$sel)
 }
-tic()
-plan(multisession)
-lasso_params <- get_parameters(df = df, group_df = group_df, block_fit = fit_lasso, block_size = 3000, max_steps = 30, rate = 0.3)
-plan(sequential)
-toc()
+fit_lasso_005 <- function(iter, df){
+  
+  sim_df <- df %>% dplyr::mutate(y = rnorm(dim(df)[1]))
+  
+  gr_lasso <- gglasso(x = as.matrix(sim_df %>% select(-y)), y = sim_df$y,
+                      group = c(1,1,2,2,3), lambda = 0.05)
+  gr_lasso_coef <- gr_lasso$beta %>% 
+    as.data.frame() %>% 
+    rownames_to_column() %>%
+    gather(key = 'lambda', value = 'coef', -rowname) %>%
+    filter(coef != 0) %>%
+    slice(1:5) 
+  gr_lasso_coef <- gr_lasso_coef %>%
+    filter(lambda == gr_lasso_coef$lambda[1])
+  gr_lasso_coef
+  gr_lasso_coef <- gr_lasso_coef %>% 
+    mutate(sel = case_when(rowname %in% c('V1','V2')~ 1, 
+                           rowname %in% c('V3','V4')~2, 
+                           rowname %in% c('V5')~ 3))
+  unique(gr_lasso_coef$sel)
+}
+
+group_df <- data.frame(var_name = c('V1', 'V2', 'V3', 'V4', 'V5'),
+                       group_name = as.integer(c(1,1,2,2,3)))
+
+fit_mb_lam_10 <- function(iter, df){
+  formula <- as.formula(y ~bols(V1, V2, lambda = 10)+
+                          bols(V3, V4, lambda = 10) +
+                          bols(V5, lambda = 10))
+  sim_df <- df %>% dplyr::mutate(y = rnorm(dim(df)[1]))
+  mb_model <- mboost(formula = formula, data = sim_df, control = boost_control(mstop = 1))
+  mb_model$xselect()
+}
+fit_mb_lam_2 <- function(iter, df){
+  formula <- as.formula(y ~bols(V1, V2, lambda = 2)+
+                          bols(V3, V4, lambda = 2) +
+                          bols(V5, lambda = 2))
+  sim_df <- df %>% dplyr::mutate(y = rnorm(dim(df)[1]))
+  mb_model <- mboost(formula = formula, data = sim_df, control = boost_control(mstop = 1))
+  mb_model$xselect()
+}
+fit_mb_df <- function(iter, df){
+  formula <- as.formula(y ~bols(V1, V2, df = 0.5)+
+                          bols(V3, V4,df = 0.5) +
+                          bols(V5,df = 0.5))
+  sim_df <- df %>% dplyr::mutate(y = rnorm(dim(df)[1]))
+  mb_model <- mboost(formula = formula, data = sim_df, control = boost_control(mstop = 1))
+  mb_model$xselect()
+}
+
+
 ### Simulations to show bias in methods
 set.seed(1)
 dat_ortho <- pracma::randortho(100, type = 'orthonormal')[,1:5,drop=F] %>%
@@ -129,67 +143,175 @@ dat_singular <- pracma::randortho(100, type = 'orthonormal')[,1:5,drop=F]%*%diag
   t(pracma::randortho(5, type = 'orthonormal')[,1:5,drop=F]) %>% as.data.frame()
 dat_independent <- matrix( rnorm(100*5,mean=0,sd=1), 100, 5) %>% as.data.frame() 
 dat_cor <- mvtnorm::rmvnorm(n=100, mean = rep(0,5), sigma = matrix(c(1,  0.9,0,0,0.5, 
-                                                                              0.9,1,  0,0,0.5,
-                                                                              0,  0,  1,0,0,
-                                                                              0,  0,  0,1,0,
-                                                                              0.5,0.5,0,0,1
-),nrow = 5)) %>%
+                                                                     0.9,1,  0,0,0.5,
+                                                                     0,  0,  1,0,0,
+                                                                     0,  0,  0,1,0,
+                                                                     0.5,0.5,0,0,1),nrow = 5)) %>%
   as.data.frame()
-run_experiment <- function(iteration, intercept = TRUE, scaled = TRUE, type = 'ortho'){
+dat_list <- list('ortho' = dat_ortho,'singular' = dat_singular,
+                 'ind' = dat_independent, 'col' = dat_cor)
+
+params_list_1 <- dat_list %>% 
+  lapply(
+    function(dat){
+      plan(multisession)
+      mb_params_df <- get_parameters(df = dat, group_df = group_df, block_size = 10000, block_fit = fit_mb_df, rate = 0.3)
+      plan(sequential)
+      plan(multisession)
+      lasso_params_01 <- get_parameters(df = dat, group_df = group_df, block_size = 10000, block_fit = fit_lasso_01, rate = 0.3)
+      plan(sequential)
+      print('0.5')
+      plan(multisession)
+      lasso_params_005 <- get_parameters(df = dat, group_df = group_df, block_size = 10000, block_fit = fit_lasso_005, rate = 0.3)
+      plan(sequential)
+      plan(multisession)
+      mb_params_lam_10 <- get_parameters(df = dat, group_df = group_df, block_size = 10000, block_fit = fit_mb_lam_10, rate = 0.8)
+      plan(sequential)
+      print('1')
+      return(list('mb_df' = mb_params_df, 'lasso_01' = lasso_params_01, 'lasso_005' = lasso_params_005,
+                  'mb_lam_10' = mb_params_lam_10))
+    }
+  )
+saveRDS(params_list_1, 'results_fixed/params_list_1.RDS')
+params_list_1 <- readRDS('results_fixed/params_list_1.RDS')
+
+run_experiment <- function(iteration){
+  ret <- data.frame()
+  for(beta_type in c('zero')){#, 'high', 'low', 'equal')){
+  for(type in c('ortho', 'ind', 'singular', 'col')){
   if(type == 'ortho'){
     dat <- dat_ortho
-  } else if(type == 'independent'){
+  } else if(type == 'ind'){
     dat <- dat_independent
   } else if(type == 'singular'){
     dat <- dat_singular
-  } else if(type == 'cor'){
+  } else if(type == 'col'){
     dat <- dat_cor
   }
-  dat <- dat %>%
-    mutate(V2 = V2*2, V4 = V4*5) %>%
-    mutate(y = rnorm(n=100))
-  if(scaled == TRUE){
+   
     dat <- dat %>%
-      mutate_all(function(x){as.numeric(scale(x))})
-  }
-  forward_aic <- lm(y ~ 1, data = dat)
-  forward_model <- add1(forward_aic, direction = "forward", scope = ~V1+V2+V3+V4+V5, test = 'F')
-  mb_model_unreg <- mboost(y ~bols(V1, V2)+bols(V3, V4) + bols(V5),
+      dplyr::mutate_all(function(x){as.numeric(scale(x))})
+    if(beta_type == 'zero'){
+      beta <- rep(0,5)
+    }
+    if(beta_type == 'high'){
+      beta <- c(0,0,0.1,0,0)
+    } 
+    if(beta_type == 'low'){
+      beta <- c(0,0,0,0,0.1)
+    } 
+    if(beta_type == 'equal'){
+      beta <- c(0.1/sqrt(2), 0.1/sqrt(2), 0.1/sqrt(2), 0.1/sqrt(2), 0.1)
+    }
+    dat <- dat %>%
+     dplyr::mutate(y = V1*beta[1]+ V2*beta[2]+ V3*beta[3]+ V4*beta[4]+ V5*beta[5]+ rnorm(n=100))
+  mb_model_unreg <- mboost::mboost(y ~bols(V1, V2)+bols(V3, V4) + bols(V5),
                      data = dat, control = boost_control(mstop = 1, nu = 1))
-  mb_model_0.5 <- mboost(y ~bols(V1, V2, df = 0.5)+
+  mb_model_0.5 <- mboost::mboost(y ~bols(V1, V2, df = 0.5) +
                            bols(V3, V4, df = 0.5) +
                            bols(V5, df = 0.5),
                          data = dat, control = boost_control(mstop = 1, nu = 1))
-  mb_model_0.1 <- mboost(y ~bols(V1, V2, df = 0.1)+
-                           bols(V3, V4, df = 0.1) +
-                           bols(V5, df = 0.1),
-                         data = dat, control = boost_control(mstop = 1, nu = 1))
-  mb_model_1 <- mboost(y ~bols(V1, V2, df = 1)+
-                           bols(V3, V4, df = 1) +
-                           bols(V5, df = 1),
-                         data = dat, control = boost_control(mstop = 1, nu = 1))
-  mb_model_lam <- mboost(y ~bols(V1, V2, lambda = 10)+
+  mb_model_0.5_sc <- mboost::mboost(dat$y ~bols(V1, V2, df = 0.5) +
+                                   bols(V3, V4, df = 0.5) +
+                                   bols(V5, df = 0.5),
+                                 data = as.data.frame(as.matrix(dat %>% select(-y))%*%diag(params_list_1[[type]]$mb_df$scaling)),
+                            control = boost_control(mstop = 1, nu = 1))
+  mb_model_lam <- mboost::mboost(y ~bols(V1, V2, lambda = 10) +
                            bols(V3, V4, lambda = 10) +
                            bols(V5, lambda = 10),
                          data = dat, control = boost_control(mstop = 1, nu = 1))
-  gr_lasso <- gglasso(x = as.matrix(dat %>% select(-y)), y = dat$y,
-                       group = c(1,1,2,2,3))
-  gr_lasso_coef <- gr_lasso$beta %>% 
-    as.data.frame() %>% 
-    rownames_to_column() %>%
-    gather(key = 'lambda', value = 'coef', -rowname) %>%
-    filter(coef != 0) %>%
-    slice(1:5) 
-  gr_lasso_coef <- gr_lasso_coef %>%
-   filter(lambda == gr_lasso_coef$lambda[1])
-  if(dim(gr_lasso_coef)[1] > 2){gr_sel <- NA} else{
-    gr_sel <- case_when(any(gr_lasso_coef$rowname %in% c('V1','V2'))~ 1, 
-                        any(gr_lasso_coef$rowname %in% c('V3'))~3, T ~ 2)
+  mb_model_lam_sc <- mboost::mboost(dat$y ~bols(V1, V2, lambda = 10) +
+                           bols(V3, V4, lambda = 10) +
+                           bols(V5, lambda = 10),
+                         data =  as.data.frame(as.matrix(dat %>% select(-y))%*%diag(params_list_1[[type]]$mb_lam_10$scaling)),
+                         control = boost_control(mstop = 1, nu = 1))
+  # gr_lasso <- gglasso(x = as.matrix(dat %>% select(-y)), y = dat$y,
+  #                      group = c(1,1,2,2,3))
+  gr_lasso_01_sc <- gglasso(x = as.matrix(dat %>% select(-y))%*%diag(params_list_1[[type]]$lasso_01$scaling),
+                         y = dat$y, group = c(1,1,2,2,3), lambda = 0.1)
+  gr_lasso_01_sc_coef <- gr_lasso_01_sc$beta %>%
+    as.data.frame() %>%
+    tibble::rownames_to_column() %>%
+    tidyr::gather(key = 'lambda', value = 'coef', -rowname) %>%
+    dplyr::filter(coef != 0) %>%
+    dplyr::slice(1:5)  %>%
+    #dplyr::filter(lambda == gr_lasso_01_sc$lambda[1]) %>%
+    dplyr::mutate(sel = case_when(rowname %in% c('V1','V2')~ 1,
+                           rowname %in% c('V3','V4')~2,
+                           rowname %in% c('V5')~ 3))
+  if(length(gr_lasso_01_sc_coef$sel) == 0){gr_lasso_01_sc_coef_sel <- NA} else{gr_lasso_01_sc_coef_sel <- gr_lasso_01_sc_coef$sel}
+  gr_lasso_01_sc_sel <- data.frame(model = 'grla01_sc', sel = unique(gr_lasso_01_sc_coef_sel), type = type, beta_type = beta_type)
+  gr_lasso_005_sc <- gglasso(x = as.matrix(dat %>% select(-y))%*%diag(params_list_1[[type]]$lasso_005$scaling),
+                         y = dat$y, group = c(1,1,2,2,3), lambda = 0.05)
+  gr_lasso_005_sc_coef <- gr_lasso_005_sc$beta %>%
+    as.data.frame() %>%
+    tibble::rownames_to_column() %>%
+    tidyr::gather(key = 'lambda', value = 'coef', -rowname) %>%
+    dplyr::filter(coef != 0) %>%
+    dplyr::slice(1:5)  %>%
+    #dplyr::filter(lambda == gr_lasso_005_sc$lambda[1]) %>%
+    dplyr::mutate(sel = case_when(rowname %in% c('V1','V2')~ 1,
+                           rowname %in% c('V3','V4')~2,
+                           rowname %in% c('V5')~ 3))
+  if(length(gr_lasso_005_sc_coef$sel) == 0){gr_lasso_005_sc_coef_sel <- NA} else{gr_lasso_005_sc_coef_sel <- gr_lasso_005_sc_coef$sel}
+  gr_lasso_005_sc_sel <- data.frame(model = 'grla005_sc', sel = unique(gr_lasso_005_sc_coef_sel), type = type, beta_type = beta_type)
+  # without scaling
+   gr_lasso_01 <- gglasso(x = as.matrix(dat %>% select(-y)),
+                         y = dat$y, group = c(1,1,2,2,3), lambda = 0.1)
+  gr_lasso_01_coef <- gr_lasso_01$beta %>%
+    as.data.frame() %>%
+    tibble::rownames_to_column() %>%
+    tidyr::gather(key = 'lambda', value = 'coef', -rowname) %>%
+    dplyr::filter(coef != 0) %>%
+    dplyr::slice(1:5)
+  gr_lasso_01_coef <- gr_lasso_01_coef %>%
+    #dplyr::filter(lambda == gr_lasso_01_coef$lambda[1]) %>%
+    dplyr::mutate(sel = case_when(rowname %in% c('V1','V2')~ 1,
+                           rowname %in% c('V3','V4')~2,
+                           rowname %in% c('V5')~ 3))
+  if(length(gr_lasso_01_coef$sel) == 0){gr_lasso_01_coef_sel <- NA} else{gr_lasso_01_coef_sel <- gr_lasso_01_coef$sel}
+  gr_lasso_01_sel <- data.frame(model = 'grla01', sel = unique(gr_lasso_01_coef_sel), type = type, beta_type = beta_type)
+  gr_lasso_005 <- gglasso(x = as.matrix(dat %>% select(-y)),
+                         y = dat$y, group = c(1,1,2,2,3), lambda = 0.05)
+  gr_lasso_005_coef <- gr_lasso_005$beta %>%
+    as.data.frame() %>%
+    tibble::rownames_to_column() %>%
+    tidyr::gather(key = 'lambda', value = 'coef', -rowname) %>%
+    dplyr::filter(coef != 0) %>%
+    dplyr::slice(1:5)
+  gr_lasso_005_coef <- gr_lasso_005_coef %>%
+    #dplyr::filter(lambda == gr_lasso_005_coef$lambda[1]) %>%
+    dplyr::mutate(sel = case_when(rowname %in% c('V1','V2')~ 1,
+                           rowname %in% c('V3','V4')~2,
+                           rowname %in% c('V5')~ 3))
+  if(length(gr_lasso_005_coef$sel) == 0){gr_lasso_005_coef_sel <- NA} else{gr_lasso_005_coef_sel <- gr_lasso_005_coef$sel}
+  gr_lasso_005_sel <- data.frame(model = 'grla005', sel = unique(gr_lasso_005_coef_sel), type = type, beta_type = beta_type)
+
+  # gr_lasso_coef <- gr_lasso$beta %>%
+  #   as.data.frame() %>%
+  #   rownames_to_column() %>%
+  #   gather(key = 'lambda', value = 'coef', -rowname) %>%
+  #   filter(coef != 0) %>%
+  #   slice(1:5)
+  # gr_lasso_coef <- gr_lasso_coef %>%
+  #  filter(lambda == gr_lasso_coef$lambda[1])
+  # if(dim(gr_lasso_coef)[1] > 2){gr_sel <- NA} else{
+  #   gr_sel <- case_when(gr_lasso_coef$rowname %in% c('V1','V2') ~ 1,
+  #                       gr_lasso_coef$rowname %in% c('V3', 'V4') ~ 2,
+  #                       rowname %in% c('V5')~ 3)
+  # }
+  ret <- ret %>% 
+    bind_rows(data.frame(model = c('mb', 'mb_0.5', 'mb_0.5_sc','mb_lam', 'mb_lam_sc'),
+              sel = c(mb_model_unreg$xselect(), mb_model_0.5$xselect(),
+                      mb_model_0.5_sc$xselect(),
+                      mb_model_lam$xselect(), mb_model_lam_sc$xselect()), type = type, beta_type = beta_type) %>%
+    rbind(gr_lasso_005_sc_sel) %>%
+    rbind(gr_lasso_005_sel) %>%
+    rbind(gr_lasso_01_sc_sel) %>%
+    rbind(gr_lasso_01_sel) %>%
+    filter(!is.na(sel)))
   }
-  ret <- data.frame(model = c('mb', 'mb_0.5', 'mb_1', 'mb_0.1', 'mb_lambda', 'grla'), 
-                    sel = c(mb_model_unreg$xselect(), mb_model_0.5$xselect(),
-                            mb_model_1$xselect(), mb_model_0.1$xselect(),
-                            mb_model_lam$xselect(), gr_sel))
+  }
   return(ret)
 }
 
@@ -197,24 +319,25 @@ library(furrr)
 plan(multisession)
 iterations <- 10000
 # Define the 12 data scenarios
-params <- tidyr::expand_grid(iteration = 1:iterations, scaled = c(TRUE,FALSE),
-                             type = c('ortho', 'independent', 'singular', 'cor'))
+params <- tidyr::expand_grid(iteration = 1:iterations)
 # run simulation in parallel
 set.seed(5)
 tictoc::tic()
 results_df <- params %>%
-  mutate( 
-    res = future_pmap(., .f = run_experiment, .progress = T, .options=furrr_options(seed = TRUE))
+  dplyr::mutate( 
+    res = future_pmap(., .f = ~run_experiment(.iteration), .progress = T,
+                      .options=furrr_options(seed = TRUE, globals = c('params_list_1', 'run_experiment', 'dat_ortho', 'dat_independent', 'dat_singular', 'dat_cor'),
+                                             packages = c('mboost', 'tidyverse', 'gglasso')))
   ) %>%
   unnest(cols = res)
 tictoc::toc()
 saveRDS(file = 'results_fixed/small_group_1_raw.RDS', results_df)
 results_df %>%
-  dplyr::group_by(scaled, model, type, sel) %>%
+  dplyr::group_by(model, type, beta_type, sel) %>%
   dplyr::tally() %>%
   dplyr::filter(!is.na(sel)) %>%
   dplyr::ungroup() %>%
-  dplyr::group_by(model) %>%
+  dplyr::group_by(model, type, beta_type) %>%
   dplyr::mutate(prop = round(n/sum(n,na.rm = T), 3)) %>%
 saveRDS(file = 'results_fixed/small_group_1.RDS')
 plan(sequential)
@@ -246,15 +369,15 @@ run_experiment <- function(iteration, intercept = TRUE, scaled = TRUE, type = 'o
     dat <- dat %>%
       mutate_all(function(x){as.numeric(scale(x))})
   }
-  mb_model_unreg <- mboost(y ~bols(V1)+bols(V2, V3, V4,V5, V6, V7,V8, V9, V10,V11, V12, V13,V14,V15),
+  mb_model_unreg <- mboost::mboost(y ~bols(V1)+bols(V2, V3, V4,V5, V6, V7,V8, V9, V10,V11, V12, V13,V14,V15),
                            data = dat, control = boost_control(mstop = 1, nu = 1))
-  mb_model_0.5 <- mboost(y ~bols(V1,df =0.5)+bols(V2, V3, V4,V5, V6, V7,V8, V9, V10,V11, V12, V13,V14,V15, df=0.5),
+  mb_model_0.5 <- mboost::mboost(y ~bols(V1,df =0.5)+bols(V2, V3, V4,V5, V6, V7,V8, V9, V10,V11, V12, V13,V14,V15, df=0.5),
                          data = dat, control = boost_control(mstop = 1, nu = 1))
-  mb_model_0.1 <- mboost(y ~bols(V1,df =0.1)+bols(V2, V3, V4,V5, V6, V7,V8, V9, V10,V11, V12, V13,V14,V15, df=0.1),
+  mb_model_0.1 <- mboost::mboost(y ~bols(V1,df =0.1)+bols(V2, V3, V4,V5, V6, V7,V8, V9, V10,V11, V12, V13,V14,V15, df=0.1),
                          data = dat, control = boost_control(mstop = 1, nu = 1))
-  mb_model_1 <- mboost(y ~bols(V1,df =1)+bols(V2, V3, V4,V5, V6, V7,V8, V9, V10,V11, V12, V13,V14,V15, df=1),
+  mb_model_1 <- mboost::mboost(y ~bols(V1,df =1)+bols(V2, V3, V4,V5, V6, V7,V8, V9, V10,V11, V12, V13,V14,V15, df=1),
                        data = dat, control = boost_control(mstop = 1, nu = 1))
-  mb_model_lam <- mboost(y ~bols(V1,lambda =10)+bols(V2, V3, V4,V5, V6, V7,V8, V9, V10,V11, V12, V13,V14,V15, lambda=10),
+  mb_model_lam <- mboost::mboost(y ~bols(V1,lambda =10)+bols(V2, V3, V4,V5, V6, V7,V8, V9, V10,V11, V12, V13,V14,V15, lambda=10),
                          data = dat, control = boost_control(mstop = 1, nu = 1))
   gr_lasso <- gglasso(x = as.matrix(dat %>% select(-y)), y = dat$y,
                       group = c(1,rep(2,15)))
